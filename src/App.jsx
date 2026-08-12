fd82517a322c5d015a3e27793da426d5d3739914
kentoo-bugging <sopheapsochetra@gmail.com>
2026-08-12 23:31:59 +0700

    HeaderPage and Kantomruy Font, Light&Dark theme

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HeaderPage from './components/HeaderPage'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900 transition-colors dark:bg-gray-950 dark:text-gray-100">
+      <HeaderPage />
+    </div>
+  )
+}
+
+export default App
